282848fcad04cae05988c10cf680a6ac73f53c2e
Max Kramer <max@maxkramer.co>
2014-01-25 14:13:16 +0000

    Added CoreData model, subclass, and cleaned up the files

---
+++ SBScrap.m
@@ -0,0 +1,19 @@
+//
+//  SBScrap.m
+//  Scrapbook
+//
+//  Created by Max Kramer on 25/01/2014.
+//  Copyright (c) 2014 Finn Gaida. All rights reserved.
+//
+
+#import "SBScrap.h"
+
+
+@implementation SBScrap
+
+@dynamic title;
+@dynamic body;
+@dynamic tags;
+@dynamic mediaURL;
+
+@end
